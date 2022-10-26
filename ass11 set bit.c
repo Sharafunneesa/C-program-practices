9ce20c4acf4d9457a6bb3b0ecb700c8b6b7d25a9
Sharafunneesa <115632205+Sharafunneesa@users.noreply.github.com>
2022-10-26 17:29:23 +0530

    Update ass11 set bit.c

--- ass11 set bit.c
+++ ass11 set bit.c
@@ -1,23 +1,23 @@
 #include<stdio.h>
 void main()
 {
-int num,i,pos;
+int num,i,pos;//declaration of variables
 printf("enter the num :");
-scanf("%d",&num);
+scanf("%d",&num);//taking number from user
 printf("enter the position to set :");
-scanf("%d",&pos);
+scanf("%d",&pos);//taking position from user to set that position
 for(i=0;i<=31;i++)
 {
   if(num & (1<<(31-i)))
     printf("1");
   else
     printf("0");
-}
+}//using for loop printing the binary of given number
 
-printf("\n");
+printf("\n");//next line
 
 for(i=0;i<=31;i++)
-num=num|(1<<pos);
+num=num|(1<<pos);//set the given position
 
 for(i=0;i<=31;i++)
 {
@@ -25,7 +25,7 @@ for(i=0;i<=31;i++)
     printf("1");
   else
     printf("0");
-}
+}//after set using for loop printing the binary of number
 printf("\n");
-printf("set pos :%d\n",num);
+printf("set pos :%d\n",num);//printng the number
 }
