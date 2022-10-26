818c2127533cb7d23e74011c6f778340b1b6f3af
Sharafunneesa <115632205+Sharafunneesa@users.noreply.github.com>
2022-10-26 17:39:25 +0530

    Update ass15 enedian.c

--- ass15 enedian.c
+++ ass15 enedian.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 void main()
 {
-int num=1;
-char *p=&num;
-if(*p==1)
-printf("the cpu is little endian\n");
+int num=1;//initializing a variable with 1
+char *p=&num;//initializing a character pointer with adress of given number
+if(*p==1)//fetching value that pointer pointing and checking it eqaul with 1
+printf("the cpu is little endian\n");//if that equal printing comment
 else
-printf("the cpu is big endian\n");
+printf("the cpu is big endian\n");// or printing comment
 p++;
 printf("%d",*p);
 }
