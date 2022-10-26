db495798e3adfb9bb891686cfb591113e7e5b3d1
Sharafunneesa <115632205+Sharafunneesa@users.noreply.github.com>
2022-10-26 17:26:51 +0530

    Update ass11 clear bit.c

--- ass11 clear bit.c
+++ ass11 clear bit.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 void main()
 {
-int num,i,pos;
+int num,i,pos;//declaration of variables
 printf("enter the num :");
-scanf("%d",&num);
+scanf("%d",&num);//taking number from user
 printf("enter the position to set :");
-scanf("%d",&pos);
+scanf("%d",&pos);//taking position from user to clear that position
 for(i=0;i<=31;i++)
 {
-  if(num & (1<<(31-i)))
+  if(num & (1<<(31-i)))//the given number is converting to binary
     printf("1");
   else
     printf("0");
@@ -17,15 +17,15 @@ for(i=0;i<=31;i++)
 printf("\n");
 
 for(i=0;i<=31;i++)
-num=num& ~(1<<pos);
+num=num& ~(1<<pos);//clar the the given position
 
 for(i=0;i<=31;i++)
 {
-  if(num&(1<<(31-i)))
+  if(num&(1<<(31-i)))//after clearing the psition convert that num into binary to print binary
     printf("1");
   else
     printf("0");
 }
 printf("\n");
-printf("set pos :%d\n",num);
+printf("set pos :%d\n",num);//print number
 }
