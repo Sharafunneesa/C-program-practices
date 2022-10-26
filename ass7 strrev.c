c255715f9ecf0a35bb27ebffcb9fff974426e875
Sharafunneesa <115632205+Sharafunneesa@users.noreply.github.com>
2022-10-26 18:07:27 +0530

    Update ass7 strrev.c

--- ass7 strrev.c
+++ ass7 strrev.c
@@ -1,23 +1,23 @@
 #include<stdio.h>
 #include<string.h>
-void str_rev(char *);
+void str_rev(char *);//declaration of function
 void main()
 {
-char d[50];
+char d[50];//declaration of variable
 printf("enter the string\n");
-scanf("%s",d);
-str_rev(d);
-printf("string reverse:%s\n",d);
+scanf("%s",d);//taking the string fro user
+str_rev(d);//calling the string reverse function
+printf("string reverse:%s\n",d);//print the reversed string
 }
 
-void str_rev(char *s)
+void str_rev(char *s)//definition of string reverse function
 {
-int i,len,temp;
-len=strlen(s);
-for(i=0;i<len/2;i++)
+int i,len,temp;//declairing the variables
+len=strlen(s);//finding the length of the string
+for(i=0;i<len/2;i++)//taking a loop to reverse
 {
-temp=s[i];
-s[i]=s[len-1-i];
-s[len-1-i]=temp;
-}
+temp=s[i];//assigning first character into temp variable
+s[i]=s[len-1-i];//assigning last character into first position
+s[len-1-i]=temp;//assigning first character into last and go on
+}//reversing all character using loop
 }
