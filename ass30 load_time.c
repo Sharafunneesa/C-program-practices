5940a898b006e9b6fe6ee0dd125497d2e3843cd0
Sharafunneesa <115632205+Sharafunneesa@users.noreply.github.com>
2022-10-27 17:27:31 +0530

    Update ass30 load_time.c

--- ass30 load_time.c
+++ ass30 load_time.c
@@ -1,13 +1,13 @@
 #include<stdio.h>  
-void main(int argc,char *argv[] )  
+void main(int argc,char *argv[] ) //taking load time input 
 { 
  int i;
- if(argc<1)
- printf("No argument passed through command line.\n");  
+ if(argc<1)//checking the given load input count is less than one
+ printf("No argument passed through command line.\n"); //if it one printing usage message
  else  
   printf("Arguments are: ");
   for(i=1;i<argc;i++)
-  printf("%s ", argv[i]);
-  printf("\n");
+  printf("%s ", argv[i]);//using for loop printing all argument passed in command line
+  printf("\n");//taking next line
 }  
      
