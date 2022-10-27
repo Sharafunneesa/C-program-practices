234151802013271408350137dad7731f573b977e
Sharafunneesa <115632205+Sharafunneesa@users.noreply.github.com>
2022-10-27 17:14:15 +0530

    Update ass2 pascal triangle.c

--- ass2 pascal triangle.c
+++ ass2 pascal triangle.c
@@ -1,21 +1,21 @@
 #include <stdio.h> 
 void main() 
 { 
- int n,c=1,k,i,j;
+ int n,c=1,k,i,j;//declaration of variables
  printf("enter number of rows: "); 
- scanf("%d",&n); 
+ scanf("%d",&n);// taking row value from the user
  for(i=0;i<n;i++) 
  { 
   for(k=1;k<=n-i;k++) 
-  printf(" "); 
+  printf(" "); //using for loop printing appropriate space 
   for(j=0;j<=i;j++) 
   { 
-   if(j==0||i==0) 
-    c=1; 
+   if(j==0||i==0)// cheking the j value and i value is zero
+    c=1; // if it zero direct put first and last value as one  
    else 
-    c=c*(i-j+1)/j; 
-   printf("%d ",c); 
+    c=c*(i-j+1)/j; //if it not calculating the vlaue in between the ones
+   printf("%d ",c); //printing the pascal triangle values
   } 
- printf("\n"); 
+ printf("\n"); //taking next line for proper triangle shape
  } 
 }
