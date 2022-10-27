1c96bd81659300c792781922c6fef9548ed25122
Sharafunneesa <115632205+Sharafunneesa@users.noreply.github.com>
2022-10-27 16:30:29 +0530

    Update ass18 atoi.c

--- ass18 atoi.c
+++ ass18 atoi.c
@@ -1,31 +1,31 @@
 #include<stdio.h>
 #include<stdlib.h>
-int my_atoi(const char *);
+int my_atoi(const char *);//declaration of user defined function my_atoi
 
 void main()
 {
 int num;
-char s[10];
+char s[10];//declaration of variables
 printf("enter the string : ");
-scanf("%s",s);
-num=my_atoi(s);
-printf("s=%s  num=%d\n",s,num);
+scanf("%s",s);//taking string from user
+num=my_atoi(s);//calling user defined my_atoi function
+printf("s=%s  num=%d\n",s,num);//printing string and converted intiger
 }
 
 int my_atoi(const char *p)
 {
- int i=0,n=0;
- if(p[0]=='-')
- i=1;
+ int i=0,n=0;//declairing variables 
+ if(p[0]=='-')//checking first element is -
+ i=1;//assigning 1 into variable i if first element is -
  for(;p[i];i++)
  {
-  if(p[i]>='0' && p[i]<='9')
-  n=n*10+p[i]-48;
+  if(p[i]>='0' && p[i]<='9')//checking the given strig is in between 0 and 9
+  n=n*10+p[i]-48;//if it is then coverting to intiger
   else
-  break;
+  break;//or break the loop
  }
- if(p[0]=='-')
- n=-n;
- return n;
+ if(p[0]=='-')//again checking the first is -
+ n=-n;//if it is the assigning negative simple to answar 
+ return n;//return the converted intiger into main function
 }
  
