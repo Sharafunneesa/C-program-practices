da4fd5d6e782dfc728a0a4cf9039f2ae20f8e048
Sharafunneesa <115632205+Sharafunneesa@users.noreply.github.com>
2022-10-27 16:06:12 +0530

    Update ass14 memory.c

--- ass14 memory.c
+++ ass14 memory.c
@@ -1,20 +1,20 @@
 #include<stdio.h>
 void main()
 {
- int array[10],*ptr,*p,i,n,memory;
+ int array[10],*ptr,*p,i,n,memory;// declaration of variables
  printf("enter the limit :");
- scanf("%d",&n);
+ scanf("%d",&n);//taking array limit from user
  printf("enter the array elements :");
  for(i=0;i<n;i++)
- scanf("%d",&array[i]);
- p=ptr=&array[0];
+ scanf("%d",&array[i]);//taking array elements from user
+ p=ptr=&array[0];//assigning base address of array int pointers
  
  printf("the array elements are :");
  for(i=0;i<n;i++)
  {
  printf("%d ",array[i]);
  ptr++;
- }
- memory=(ptr-p)*4;
- printf("\nmemory allocated for intiger array is %d bytes\n",memory);
+ }//printing array elements in screen
+ memory=(ptr-p)*4;//calculating memory for array allocation
+ printf("\nmemory allocated for intiger array is %d bytes\n",memory);//print the memory size
 }
