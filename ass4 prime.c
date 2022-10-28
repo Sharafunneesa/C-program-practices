369557f18c491ee7b37c96e465f0015a7defbfb0
Sharafunneesa <115632205+Sharafunneesa@users.noreply.github.com>
2022-10-28 14:50:41 +0530

    Update ass4 prime.c

--- ass4 prime.c
+++ ass4 prime.c
@@ -5,20 +5,20 @@ void main()
  printf("enter the number to find the prime factor:");
  scanf("%d",&num);//taking number from user
  printf("prime factors are:");
- for(i=2;i<=num/2;i++)
+ for(i=2;i<=num/2;i++)//taking a for loop for finding the prime number
  {
-   if(num%i==0)
+   if(num%i==0)//checking the given number is prime or not
    {
-    for(j=2;j<=i/2;j++)
+    for(j=2;j<=i/2;j++)//if that prime then take a for loop for finding the factors
     {
-      if(i%j==0)
+      if(i%j==0)//if the i value is modulus by j value is equal to 0
       {
-       flag=1;
-       break;
+       flag=1;//then setting the flag
+       break;//then break the loop
       }
     }
-    if(flag==0)
-     printf("%d ",i);
+    if(flag==0)//if the given flag is not setting to one 
+     printf("%d ",i);//then print the i value that is the prim factor
    }
  }
 }
