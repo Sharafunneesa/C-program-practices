6fb51223b6b7814cd4d0f4aaad8559ac4fbfc59a
Sharafunneesa <115632205+Sharafunneesa@users.noreply.github.com>
2022-10-28 12:51:14 +0530

    Update ass10 reverse bit.c

--- ass10 reverse bit.c
+++ ass10 reverse bit.c
@@ -1,30 +1,30 @@
 #include<stdio.h>
 void main()
 {
-int num,r=0,i,p;
-printf("enter the num :");
-scanf("%d",&num);
-for(i=0;i<=31;i++)
-{
-  if(num & (1<<(31-i)))
+ int num,r=0,i,p;//declaration of variables
+ printf("enter the num :");
+ scanf("%d",&num);//taking number from user
+ for(i=0;i<=31;i++)
+ {
+  if(num & (1<<(31-i)))//checking the given position set or clear
     printf("1");
   else
     printf("0");
-}
-printf("\n");
-p=sizeof(num)*8;
-for(i=0;i<p;i++)
-{
-if((num&(1<<i)))
-r=r| 1<<((p-1)-i);
-}
-for(i=0;i<=31;i++)
-{
-  if(r&(1<<(31-i)))
-    printf("1");
-  else
-    printf("0");
-}
-printf("\n");
-printf("reverse num :%d\n",r);
+ }//using for loop print the binary of given number
+ printf("\n");
+ p=sizeof(num)*8;//
+ for(i=0;i<p;i++)
+ {
+  if((num&(1<<i)))
+    r=r| 1<<((p-1)-i);
+ }
+ for(i=0;i<=31;i++)
+ {
+   if(r&(1<<(31-i)))
+     printf("1");
+   else
+     printf("0");
+ }
+ printf("\n");
+ printf("reverse num :%d\n",r);
 }
