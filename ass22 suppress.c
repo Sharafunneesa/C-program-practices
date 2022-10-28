7b42a67fab1e2d1ad6334c7eb4106d17fbb2cec9
Sharafunneesa <115632205+Sharafunneesa@users.noreply.github.com>
2022-10-28 14:46:24 +0530

    Update ass22 suppress.c

--- ass22 suppress.c
+++ ass22 suppress.c
@@ -6,29 +6,28 @@ int main()
  char s[50];
  char ch;
  printf("enter the string : ");
- scanf(" %[^\n]",s);
+ scanf(" %[^\n]",s);//taking the string from user
  printf("enter the char to suppress : ");
- scanf(" %c", &ch);
- suppress_in_string(s,ch);
+ scanf(" %c", &ch);//taking the character to suppress from user
+ suppress_in_string(s,ch);//calling the user defined suppress_in_string function
  }
  
- void suppress_in_string(char *str,char c)
+ void suppress_in_string(char *str,char c)//definition of user defined function
  {
-  int len,i,j;
-  len = strlen(str);
-  printf("%c", c);
-  for(i = 0; i < len; i++)
+  int len,i,j;//declaiaring the variables
+  len = strlen(str);//calculating the string length and assignning to len variable
+  for(i = 0; i < len; i++)//taking a loop to rotate from first character to last of string
   {
-   if(str[i] == c)
+   if(str[i] == c)//checking every character with the user taken character
    {
-    for(j = i; j < len; j++)
+    for(j = i; j < len; j++)//if those character's are same then suppress that character
     {
-     str[j] = str[j + 1];
+     str[j] = str[j + 1];//move the next character into the posiion of suppressed character
     }
-    len--;
-    i--;	
+    len--;//decrease the string length for next itration
+    i--;	//decrease the i value for next iteration
    } 
   }
- printf("the string is :%s\n",str);
+ printf("the string is :%s\n",str);//printing the string after suppressing the character
  }
  
