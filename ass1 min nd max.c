c18bedae08c2a8b10cdeb635cdcd09c7555a70fd
Sharafunneesa <115632205+Sharafunneesa@users.noreply.github.com>
2023-05-24 18:03:36 +0530

    Update ass1 min nd max.c

--- ass1 min nd max.c
+++ ass1 min nd max.c
@@ -1,20 +1,20 @@
 #include<stdio.h>
 int main()
 {
- int a[50],i,n,min,max;//declarationof variables
+ int a[50],i,n,min,max;                     //declarationof variables
  printf("Enter size of the array : ");
- scanf("%d",&n);//taking array size from user
+ scanf("%d",&n);                            //taking array size from user
  printf("Enter elements in array : ");
  for(i=0;i<n;i++)
-   scanf("%d",&a[i]);//taking array elements from user
- min=max=a[0];//assigning array first element as minimum and maximum
+   scanf("%d",&a[i]);                       //taking array elements from user
+ min=max=a[0];                              //assigning array first element as minimum and maximum
  for(i=1;i<n;i++)
  {
-  if(min>=a[i])//checking every elemenet less than or equal to minimum value
-    min=a[i];// if that less than, then assigning to min
-  if(max<=a[i])//checking every elemrnt greater than or equal to maximum value
-    max=a[i]; // if that grater then assigning     
+  if(min>=a[i])                             //checking every elemenet less than or equal to minimum value
+    min=a[i];                               // if that less than, then assigning to min
+  if(max<=a[i])                             //checking every elemrnt greater than or equal to maximum value
+    max=a[i];                               // if that grater then assigning     
  }
- printf("minimum of array is : %d\n",min);// printing minimum value
- printf("maximum of array is : %d\n",max);//printing maximum value
+ printf("minimum of array is : %d\n",min);  // printing minimum value
+ printf("maximum of array is : %d\n",max);  //printing maximum value
 }
